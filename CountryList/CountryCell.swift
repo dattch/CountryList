59debe7dc97f057a9b4496aaeb680d58da3dad88
Morgan Dock <morgan.dock@gmail.com>
2019-04-26 11:28:01 -0700

    update to swift 5

--- CountryList/CountryCell.swift
+++ CountryList/CountryCell.swift
@@ -28,7 +28,7 @@ class CountryCell: UITableViewCell {
         }
     }
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
         
