6e9063f97803e160f9f56d6e3076c38a2f6bdbc5
Morgan Dock <morgan.dock@gmail.com>
2019-04-26 11:29:13 -0700

    add demo for ignoring some countries

--- Example/CountryListExample/ViewController.swift
+++ Example/CountryListExample/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, CountryListDelegate {
     @IBOutlet weak var selectedCountryLabel: UILabel!
 
     //Here, you can localize the title and the search placeholder strings if needed
-    var countryList = CountryList(title: "Country List", searchPlaceholder: "Search")
+    var countryList = CountryList(title: "Country List", searchPlaceholder: "Search", ignoreCountryCodes: ["AO", "AZ", "BH", "BJ", "BF", "CV", "CG", "FJ", "GW", "KG", "LA", "MG", "ML", "MU", "FM", "MN", "MZ", "NP", "NI", "NE", "PW", "PA", "PG", "PY", "SC", "SR", "TJ", "UG", "UZ"])
     
     override func viewDidLoad() {
         super.viewDidLoad()
